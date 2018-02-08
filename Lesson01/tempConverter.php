6288e82a1a4ee0b6ffbc4fd3357a512a717b2978
Kevin <stoutkev3@gmail.com>
2018-02-07 20:26:24 -0500

    changed some of the style of the table

--- Lesson01/tempConverter.php
+++ Lesson01/tempConverter.php
@@ -1,13 +1,14 @@
 <?php
 
 $celsius = 0;
+
 echo"<table>";
 echo "<tr><th>Celsius</th><th>Fahrenheit</th></tr>";
 
 for ($i=0; $i < 10; $i++) { 
     
    $fahrenheit = 9/5*($celsius + 32);
-   echo "<tr><td>$celsius</td><td>$fahrenheit</td></tr>";
+   echo "<tr><td style=\"text-align:center\">$celsius</td><td style=\"text-align:center\">$fahrenheit</td></tr>";
    $celsius += 5;   
 }
 
