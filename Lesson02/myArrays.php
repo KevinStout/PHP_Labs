344ba057b2d935f472ac431a5aa39ecc917c97b6
Kevin <stoutkev3@gmail.com>
2018-02-16 15:28:03 -0500

    setting up exercise 4

--- Lesson02/myArrays.php
+++ Lesson02/myArrays.php
@@ -1,5 +1,14 @@
 <?php
 
-
+$price = array(100.00, 200.00, 300.00, 400.00, 500.00, 600.00);
+$categories = array(0=>"All Mountain Skis", 1=>"Powder Skis", 2=>"Carving Skis", 3=>"Twin Tip Skis", 4=>"Park and Pipe Skis", 5=>"Alpine Touring Skis");
+$companyAndSkis = array(
+    array("Armada", "ARV", "ARW", "Tracer", "Invictus", "Victa"),
+    array("Volkl", "Revolt", "Mantra", "Confession", "Kenja", "Kendo"),
+    array("Atomic", "Vantage", "Punx", "Backland", "Redster", "Cloud"),
+    array("Rossignol", "Sassy", "Temptation", "Sky7", "Sin", "Smash", ),
+    array("Nordica", "Enforcer", "Astral", "Soul Rider", "NRGY", "Ace"),
+    array("Head", "Kore", "Rally", "Joy", "Instinct", "Monster"), 
+);
 
 ?>
